c18fea683aeb5e801454809be905fcb7d20b0600
misterwolf1621 <felix-wol2808@hotmail.de>
2023-01-19 21:11:39 +0100

    first commit

---
+++ views/index.js
@@ -0,0 +1,19 @@
+console.log('HALLO');
+
+function direction(dir) {
+    if(dir == 'f') {
+
+    }
+
+    if(dir == 'b') {
+        
+    }
+
+    if(dir == 'l') {
+        
+    }
+
+    if(dir == 'r') {
+        
+    }
+}
