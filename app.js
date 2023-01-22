1b9825b85794ae0be6863a4d79201f812859827a
misterwolf1621 <70652723+misterwolf1621@users.noreply.github.com>
2023-01-22 19:50:33 +0100

    style sheet added

--- app.js
+++ app.js
@@ -6,19 +6,15 @@ const xbee = require('xbee');
 
 const app = express();
 const port = 7113;
-
+/*
 const { SerialPort } = require('serialport');
 const comPort1 = new SerialPort({
 path: 'COM3',
 baudRate: 9600,
-/*
-dataBits: 8,
-stopBits: 1,
-parity: 'none', */
 });
-
+*/
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + '/public'));
 var leftSpeed = 0;
 var rightSpeed = 0;
 
@@ -26,6 +22,8 @@ var rightSpeed = 0;
 app.get("/", (req, res) => {
     res.render("../views/index.ejs");
 });
+
+
 
 app.get('/dir', (req, res)=> {
     var dir = req.query('dir');
