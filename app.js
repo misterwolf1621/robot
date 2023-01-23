0cf4130efedb830c531bbccea43e4b0fac101d7b
misterwolf1621 <felix-wol2808@hotmail.de>
2023-01-23 15:32:41 +0100

    waffle animation added on first load

--- app.js
+++ app.js
@@ -3,40 +3,51 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const xbee = require('xbee');
-
 const app = express();
 const port = 7114;
-
+/*
 const { SerialPort } = require('serialport');
 const comPort = new SerialPort({
 path: 'COM3',
 baudRate: 9600,
-
-dataBits: 8,
-stopBits: 1,
-parity: 'none', 
 });
-
+*/
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+var firstLogin = false;
 var leftSpeed = 0.5;
 var rightSpeed = 0.5;
 var gndspeed = 0;
 
-app.get("/", (req, res) => {
-    res.render("../views/index.ejs");
+app.get("/", load, (req, res) => {
+    
+        res.render('../views/index.ejs');
+    
 });
 
+function load(req, res, next) {
+    if(!firstLogin) {
+        res.render('../views/loading.ejs');
+        firstLogin = true;
+        
+
+        console.log("first");
+    } else {
+        next();
+    }
+    
+    
+}
 
 
 app.get('/dir', (req, res)=> {
     var dir = req.query.dir;
 
     if(dir == "f") {
-        gndspeed = gndspeed + 10;
+        gndspeed = gndspeed + 20;
     } else if(dir == "b") {
-        gndspeed = gndspeed - 10;
+        gndspeed = gndspeed - 20;
     } else if(dir == "r") {
         leftSpeed = leftSpeed+ 0.1;
         rightSpeed = rightSpeed- 0.1;
@@ -73,11 +84,11 @@ app.get('/dir', (req, res)=> {
     comPort.write(spd.toString());
     
 });
-
+/*
 comPort.on('readable', function () {
     console.log('Data:', comPort.read())
   })
-
+*/
 app.listen(port, console.log('listening on port ' + port));
 
 //Hallo
